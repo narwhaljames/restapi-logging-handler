74560d18753658ffc70d1053b2bd2b69958b856d
RJ Gilligan <rj.gilligan@nrgenergy.com>
2014-11-07 10:51:28 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ import os
 from setuptools import setup
 
 
-def read(*paths):
-    """Build a file path from *paths* and return the contents."""
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
 
 setup(
     name='restapi-logging-handler',
-    version='0.1.1',
+    version='0.1.2',
     description='A handler for the python logging module that allows \
         sending logs through a REST-ful API. With Loggly support.',
-    long_description=(read('README.md')),
+    long_description=description,
     packages=['restapi_logging_handler'],
     install_requires=['requests'],
     author='RJ Gilligan',
     author_email='rj.gilligan@nrgnergy.com',
     url='https://github.com/narwhaljames/restapi-logging-handler.git',
-    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1.1',
+    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1.2',
     keywords=['rest', 'api', 'logging', 'handler', 'loggly'],
     classifiers=[],
     license='MIT',
