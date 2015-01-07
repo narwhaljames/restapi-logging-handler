3caff088ee17154284ed271f9ef774d47ff3ab68
RJ Gilligan <rj.gilligan@nrgenergy.com>
2015-01-07 14:42:33 -0500

    Updated for py3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except (IOError, ImportError):
 
 setup(
     name='restapi-logging-handler',
-    version='0.1.5',
+    version='0.1.6',
     description='A handler for the python logging module that allows \
         sending logs through a REST-ful API. With Loggly support.',
     long_description=description,
@@ -19,7 +19,7 @@ setup(
     author='RJ Gilligan',
     author_email='rj.gilligan@nrgnergy.com',
     url='https://github.com/narwhaljames/restapi-logging-handler.git',
-    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1.5',
+    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1.6',
     keywords=['rest', 'api', 'logging', 'handler', 'loggly'],
     classifiers=[],
     license='MIT',
